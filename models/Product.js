aaac0d36b815ee33628e4bcdd27fd0f82d21312f
Loretta Yong <loretta0386@hotmail.com>
2026-01-31 07:36:41 +0800

    update backend

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema(
   {
     name: {type: String, required: true},
     price: {type: Number},
+    stock: {type: Number},
     supplier: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Supplier",
