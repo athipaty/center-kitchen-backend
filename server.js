5e671cfb9b7be60017da40bb90dddf5cf788ef1f
Ting Tong <103483140+athipaty@users.noreply.github.com>
2026-01-31 08:26:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
       "http://localhost:5173",
       "https://athipaty-center-kitchen-frontend.vercel.app",
       "https://supplier-nine.vercel.app",
+      "https://inventory-ten-o-mega.vercel.app",
     ],
     credentials: true,
   })
