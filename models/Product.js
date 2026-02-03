26983db9fdb4ab8097298516a79bd15dd6553e9f
Loretta Yong <loretta0386@hotmail.com>
2026-02-03 11:33:24 +0800

    update unit

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ const ProductSchema = new mongoose.Schema(
     name: {type: String, required: true},
     price: {type: Number},
     stock: {type: Number},
+    unit: {type: String},
     supplier: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Supplier",
