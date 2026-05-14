0aa456af1eb0b36e83b9a798abe48bb525257119
Loretta Yong <loretta0386@hotmail.com>
2026-05-14 18:09:50 +0800

    test

--- routes/abt.js
+++ routes/abt.js
@@ -56,6 +56,7 @@ function getUploadPdf() {
       storage: multerS3({
         s3: b2Client,
         bucket: B2_BUCKET,
+        acl: false,
         contentType: (_req, _file, cb) => cb(null, 'application/pdf'),
         key: (req, file, cb) => {
           const safe = file.originalname.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9._-]/g, '')
